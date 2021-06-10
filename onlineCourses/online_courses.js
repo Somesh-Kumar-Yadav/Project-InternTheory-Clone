ac92cc728655b58be34e7deb252c02b9440a416d
Somesh Kumar Yadav <somesh_nj2_076@masai.school>
2021-06-10 12:34:08 +0530

    Modified landing page and added functionality to onlinecourses page

--- onlineCourses/online_courses.js
+++ onlineCourses/online_courses.js
@@ -1,100 +1,179 @@
-let cart_1_button = document.getElementById("cart_1_button")
-cart_1_button.addEventListener("click", function () {
-    let cart_1 = document.getElementById("cart_1")
-    cart_1_button.innerText = "CHECKOUT >>";
-
-    console.log(cart_1);
-    
-
-})
-
-
-let cart_2_button = document.getElementById("cart_2_button")
-cart_2_button.addEventListener("click", function () {
-    let cart_2 = document.getElementById("cart_2")
-    cart_2_button.innerText = "CHECKOUT >>";
-
-    console.log(cart_2);
-    
-})
-
-
-let cart_3_button = document.getElementById("cart_3_button")
-cart_3_button.addEventListener("click", function () {
-    let cart_3 = document.getElementById("cart_3")
-    cart_3_button.innerText = "CHECKOUT >>";
-
-    console.log(cart_3);
-    
-})
-
-
-let cart_4_button = document.getElementById("cart_4_button")
-cart_4_button.addEventListener("click", function () {
-    let cart_4 = document.getElementById("cart_4")
-    cart_4_button.innerText = "CHECKOUT >>";
-
-    console.log(cart_4);
-    
-})
-
-
-let cart_5_button = document.getElementById("cart_5_button")
-cart_5_button.addEventListener("click", function () {
-    let cart_5 = document.getElementById("cart_5")
-    cart_5_button.innerText = "CHECKOUT >>";
-
-    console.log(cart_5);
-    
-})
-
-
-let cart_6_button = document.getElementById("cart_6_button")
-cart_6_button.addEventListener("click", function () {
-    let cart_6 = document.getElementById("cart_6")
-    cart_6_button.innerText = "CHECKOUT >>";
-
-    console.log(cart_6);
-    
-})
-
-
-let cart_7_button = document.getElementById("cart_7_button")
-cart_7_button.addEventListener("click", function () {
-    let cart_7 = document.getElementById("cart_7")
-    cart_7_button.innerText = "CHECKOUT >>";
-
-    console.log(cart_7);
-    
-})
-
-
-let cart_8_button = document.getElementById("cart_8_button")
-cart_8_button.addEventListener("click", function () {
-    let cart_8 = document.getElementById("cart_8")
-    cart_8_button.innerText = "CHECKOUT >>";
-
-    console.log(cart_8);
-    
-})
-
-
-let cart_9_button = document.getElementById("cart_9_button")
-cart_9_button.addEventListener("click", function () {
-    let cart_9 = document.getElementById("cart_9")
-    cart_9_button.innerText = "CHECKOUT >>";
-
-    console.log(cart_9);
-    
-})
-
-
-let cart_10_button = document.getElementById("cart_10_button")
-cart_10_button.addEventListener("click", function () {
-    let cart_10 = document.getElementById("cart_10")
-    cart_10_button.innerText = "CHECKOUT >>";
-
-    console.log(cart_10);
-    
-})
-
+let total_block = document.getElementById("total");
+let total;
+total = localStorage.getItem("total");
+if (total == null) {
+  total = 0;
+}
+let avail_courses;
+avail_courses = localStorage.getItem("avail_courses");
+if (avail_courses == null) {
+  avail_courses = [
+    {
+      title: "IT Stater Pack(4 Courses)",
+      descp: "Learn the most demanded skills in the IT industry today Sig...",
+      emi: "EMI Starting at ₹ 915/month",
+      old_price: "32999",
+      new_price: "15999",
+      img_source:
+        "https://assets.interntheory.com/creative/courses/thumbnails/it.jpg",
+      btn_option: "ADD TO CART",
+    },
+    {
+      title: "Finance Starter Pack (3 Courses)",
+      descp: "Learn the most demanded skills in the Finance industry today ...",
+      emi: "EMI Starting at ₹ 687/month",
+      old_price: "23999",
+      new_price: "11999",
+      img_source:
+        "https://assets.interntheory.com/creative/courses/thumbnails/finthumbnail.jpg",
+      btn_option: "ADD TO CART",
+    },
+    {
+      title: "Digital Marketing Course",
+      descp: "Learn the art of digital marketing. Sign up for this online ...",
+      emi: "EMI Starting at ₹ 343/month",
+      old_price: "11999",
+      new_price: "5999",
+      img_source:
+        "https://assets.interntheory.com/creative/courses/thumbnails/digital-marketing-course.png",
+      btn_option: "ADD TO CART",
+    },
+    {
+      title: "Stock Market Course",
+      descp: "Learn how to trade and invest in stock markets. Enrol for th ...",
+      emi: "EMI Starting at ₹ 343/month",
+      old_price: "11999",
+      new_price: "5999",
+      img_source:
+        "https://assets.interntheory.com/creative/courses/thumbnails/stock-market-course.png",
+      btn_option: "ADD TO CART",
+    },
+    {
+      title: "Data Analytics With R Programming Course",
+      descp: "Learn data science online. Join this online data analytics c ...",
+      emi: "EMI Starting at ₹ 343/month",
+      old_price: "11999",
+      new_price: "5999",
+      img_source:
+        "https://assets.interntheory.com/creative/courses/thumbnails/data-analytics-r-programming-course.png",
+      btn_option: "ADD TO CART",
+    },
+    {
+      title: "Web Development Course",
+      descp: "Learn the art of web design and web development. Join this o ...",
+      emi: "EMI Starting at ₹ 299/month",
+      old_price: "7999",
+      new_price: "3999",
+      img_source:
+        "https://assets.interntheory.com/creative/courses/thumbnails/web-development-course.png",
+      btn_option: "ADD TO CART",
+    },
+    {
+      title: "Tally + GST Course",
+      descp: "Sign up for this online tally and gst course and learn how t ...",
+      emi: "EMI Starting at ₹ 229/month",
+      old_price: "7999",
+      new_price: "3999",
+      img_source:
+        "https://assets.interntheory.com/creative/courses/thumbnails/tally-gst-course.png",
+      btn_option: "ADD TO CART",
+    },
+    {
+      title: "Android App Development Using Kotlin Course",
+      descp: "Learn the art of android app design and web development. Joi ...",
+      emi: "EMI Starting at ₹ 229/month",
+      old_price: "7999",
+      new_price: "3999",
+      img_source:
+        "https://assets.interntheory.com/creative/courses/thumbnails/android-app-development-kotlin-course.png",
+      btn_option: "ADD TO CART",
+    },
+    {
+      title: "Advanced Excel Course",
+      descp: "Master MS Excel. Take the online ms excel course and learn t ...",
+      emi: "EMI Starting at ₹ 172/month",
+      old_price: "5999",
+      new_price: "2999",
+      img_source:
+        "https://assets.interntheory.com/creative/courses/thumbnails/advanced-excel-course.png",
+      btn_option: "ADD TO CART",
+    },
+    {
+      title: "R Programming Course",
+      descp: "Learn r programming online. Sign up for this online r progra ...",
+      emi: "EMI Starting at ₹ 172/month",
+      old_price: "5999",
+      new_price: "2999",
+      img_source:
+        "https://assets.interntheory.com/creative/courses/thumbnails/r-programming-course.png",
+      btn_option: "ADD TO CART",
+    },
+  ];
+} else {
+  avail_courses = JSON.parse(avail_courses);
+}
+let carts = document.getElementById("carts");
+for (var i = 0; i < avail_courses.length; i++) {
+  let cart = document.createElement("div");
+  cart.setAttribute("class", "cart");
+  cart.setAttribute("id", `${i}`);
+  cart.innerHTML = `
+            <div>
+              <img
+                style="height: 115px"
+                src=${avail_courses[i].img_source}
+                alt="image"
+              />
+            </div>
+            <div class="padding">
+              <div>
+                <h4 class="color_black">${avail_courses[i].title}</h4>
+                <p class="color_gray">
+                 ${avail_courses[i].descp}
+                </p>
+              </div>
+              <div class="border_bottom"></div>
+              <div class="left_align">
+                <h5 class="color_gray line_through">₹ ${avail_courses[i].old_price}</h5>
+                <h3 class="color_black">₹ ${avail_courses[i].new_price}</h3>
+              </div>
+              <div>
+                <a class="left_align color_yellow" href="#"
+                  >${avail_courses[i].emi}
+                  <span class="border_radius color_yellow">?</span>
+                </a>
+              </div>
+            </div>
+            <div class="left_align">
+              <a class="color_green" href="#">KNOW MORE</a>
+              <button class="button" id="button_${i}" onclick="checkOut(${i})">${avail_courses[i].btn_option}</button>
+            </div>`;
+  carts.append(cart);
+}
+function checkOut(id) {
+  avail_courses = JSON.parse(localStorage.getItem("avail_courses"));
+  let status = document.getElementById("button_" + id);
+  if (status.innerText == "ADD TO CART") {
+    status.innerText = "CHECKOUT >>";
+    total = Number(total) + 1;
+    avail_courses[id].btn_option = "CHECKOUT >>";
+    localStorage.setItem("avail_courses", JSON.stringify(avail_courses));
+    localStorage.setItem("total", total);
+  } else {
+    window.location.href = "cart.html";
+  }
+  if (total != 0) {
+    total_block.innerHTML = `${total}`;
+    total_block.style.visibility = "visible";
+  } else {
+    total_block.style.visibility = "hidden";
+  }
+}
+if (total != 0) {
+  total_block.innerHTML = `${total}`;
+  total_block.style.visibility = "visible";
+} else {
+  total_block.style.visibility = "hidden";
+}
+localStorage.setItem("total", total);
+localStorage.setItem("avail_courses", JSON.stringify(avail_courses));
